6c0cd19abc99890ab284bb8b062ccad7d9091dc7
deor1496 <deyxon.ortiz@gmail.com>
2020-04-21 10:33:32 -0400

    Se le agrego el componente de navegacion llamado header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
 import  HomePage  from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shopPage.component";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>This is the hat page</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
       
     </div>
